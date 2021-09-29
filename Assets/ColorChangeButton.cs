dea2f93ce7a9a9b6db4dd9f43c1f4c0a1d67d35d
Szymon Kazmierczak <skazmierczak@zendesk.com>
2021-09-29 15:53:59 +0100

    init

---
+++ Assets/ColorChangeButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ColorChangeButton : MonoBehaviour
+{
+    public Theme theme;
+
+    private Image _buttonImage;
+    private Text _textComponent;
+
+    private void Start()
+    {
+        // Cache references
+        _textComponent = GetComponentInChildren<Text>();
+        _buttonImage = gameObject.GetComponent<Image>();
+
+        // Set Initial Values
+        UpdateValues();
+
+        // Subscribe to event in case the values are changed and update them
+        theme.OnColorChanged += UpdateValues;
+    }
+
+    private void UpdateValues()
+    {
+        _buttonImage.color = theme.ButtonColor;
+        _textComponent.text = $"Click me to change my colour! My colour is {theme.ButtonColor}";
+    }
+
+    public void ChangeBackgroundColor()
+    {
+        theme.ChangeButtonColor(Random.ColorHSV());
+    }
+}
---
+++ Assets/Theme.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Create Theme", fileName = "Theme")]
+public class Theme : ScriptableObject
+{
+    public event Action OnColorChanged;
+
+    public Color ButtonColor
+    {
+        get => _buttonColor;
+        private set
+        {
+            _buttonColor = value;
+            OnColorChanged?.Invoke();;
+        }
+    }
+
+    private Color _buttonColor;
+
+    // Either this, expose setter or another solution like being able to set whole theme in one go? Multiple colors at once? up to you
+
+    // Could have overloads for different types of color setting, like the html parser we have in your branch?
+    public void ChangeButtonColor(Color color)
+    {
+        ButtonColor = color;
+    }
+}
